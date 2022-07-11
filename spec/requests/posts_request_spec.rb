5301925c150c6162fd480dc4322a3cb317808c9b
tafaramafemba <tafara.maf@gmail.com>
2022-07-11 19:08:01 +0200

    fix linter errors

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe 'Test index action', type: :request do
   before(:example) do
     @user = User.create!(name: 'Tom', photo: 'https://unsplash.com/photos/F_0BxGuVvo', bio: 'Teacher from Mexico.')
     get user_posts_path(@user)
-    end
-   it "is a success" do
-     expect(response).to have_http_status(:ok)
-   end
-   it "renders 'index' template" do
-     expect(response).to render_template('index')
-   end
+  end
+  it 'is a success' do
+    expect(response).to have_http_status(:ok)
+  end
+  it "renders 'index' template" do
+    expect(response).to render_template('index')
+  end
 
-   it 'should display the correct placeholder text' do
+  it 'should display the correct placeholder text' do
     expect(response.body).to include('Here is a list of all posts by a user')
   end
 end
@@ -20,17 +20,18 @@ end
 RSpec.describe 'Test index action' do
   before(:example) do
     @user = User.create!(name: 'Tom', photo: 'https://unsplash.com/photos/F_0BxGuVvo', bio: 'Teacher from Mexico.')
-    @post =  @user.posts.create!(title: 'The ephicacy of things', text: 'Wow the ephicacy of things', comments_counter: 0, likes_counter: 0)
+    @post = @user.posts.create!(title: 'The ephicacy of things', text: 'Wow the ephicacy of things',
+                                comments_counter: 0, likes_counter: 0)
     get user_post_path(@user, @post)
-    end
-   it "is a success" do
-     expect(response).to have_http_status(:ok)
-   end
-   it "renders 'show' template" do
-     expect(response).to render_template(:show)
-   end
+  end
+  it 'is a success' do
+    expect(response).to have_http_status(:ok)
+  end
+  it "renders 'show' template" do
+    expect(response).to render_template(:show)
+  end
 
-   it 'should display the correct placeholder text' do
+  it 'should display the correct placeholder text' do
     expect(response.body).to include('Here is a post by username')
   end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Test index action', type: :request do
   before(:example) { get users_path }
-   it "is a success" do
-     expect(response).to have_http_status(:ok)
-   end
-   it "renders 'index' template" do
-     expect(response).to render_template('index')
-   end
+  it 'is a success' do
+    expect(response).to have_http_status(:ok)
+  end
+  it "renders 'index' template" do
+    expect(response).to render_template('index')
+  end
 
-   it 'should display the correct placeholder text' do
+  it 'should display the correct placeholder text' do
     expect(response.body).to include('Here is a list of all users')
   end
 end
 
 RSpec.describe 'Test show action' do
   before(:example) do
-  @user = User.create!(name: 'Tom', photo: 'https://unsplash.com/photos/F_0BxGuVvo', bio: 'Teacher from Mexico.')
-  get user_path(@user)
+    @user = User.create!(name: 'Tom', photo: 'https://unsplash.com/photos/F_0BxGuVvo', bio: 'Teacher from Mexico.')
+    get user_path(@user)
   end
 
-  it "is a success" do
+  it 'is a success' do
     expect(response).to have_http_status(:ok)
   end
   it "renders 'show' template" do
@@ -28,7 +28,6 @@ RSpec.describe 'Test show action' do
   end
 
   it 'should display the correct placeholder text' do
-   expect(response.body).to include('Here is a user')
- end
+    expect(response.body).to include('Here is a user')
+  end
 end
-
