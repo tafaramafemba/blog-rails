34acde556a821654339c49c4c970dad63d227222
tafaramafemba <tafara.maf@gmail.com>
2022-07-14 11:32:40 +0200

    add like and comment methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,20 +8,41 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = Comment.where(post_id: params[:id]).where(author_id: params[:user_id])
   end
-end
 
-def new
-  @post = Post.new
-  @user = current_user
-end
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+  
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to user_post_path(@post.author_id, @post), notice: 'Post was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 
-def create
-  @post = Post.new(post_params)
-  @post.user = current_user
-  if @post.save
-    puts @post
+  def like
+    @post = Post.find(params[:id])
+    Like.new(user: current_user, post: @post).save
     redirect_to user_posts_path(current_user)
-  else
-    render :new
+  end
+
+  def comment
+    @post = Post.find(params[:id])
+    p params
+    @comment = Comment.new(user: current_user, post: @post, text: params[:text])
+    @comment.save
+    redirect_to user_posts_path(current_user)
   end
 end
+
