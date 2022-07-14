19c22cb406a9cdea82f7a4cafcfe21e2b8ff3d05
tafaramafemba <tafara.maf@gmail.com>
2022-07-14 08:59:47 +0200

    add new and create routes for posts, likes and comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create]
+    resources :likes, only: [:create]
+    resources :comments, only: [:new, :create]
   end
 end
