560ffa355e7ca180af80e96222d7d2b182a9e84f
tafaramafemba <tafara.maf@gmail.com>
2022-07-22 00:13:06 +0200

    add index and create methods in post controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,15 +1,37 @@
 module Api
   module V1
     class CommentsController < ApplicationController
-      def index
-  
-      end
+        def index
+          @comments = Comment.where(post_id: params[:post_id]).where(author_id: params[:user_id]).order(:created_at)
+          render json: {
+            success: true, message: "Successfully loaded comments", data: { comments: @comments }
+          }
+        end
 
-      def create
-        
-      end
-      
-  end
+        def create
+          @post = Post.find(params[:post_id])
+          @comment = @post.comments.create(comment_params, author_id: params[:user_id])
+
+          if @comment.save
+            render json: { success: true, message: 'Successfully created comment', data: { comment: @comment } }, status: :created
+          else
+            render json: { success: false, errors: @comment.errors }, status: :unprocessable_entity
+          end
+        end
+        rescue ActiveRecord::RecordNotFound
+          no_record(params[:post_id])
+        end
+
+        private
+
+        def no_record(post)
+          render json: { success: false, message: "This post: #{post} does not exist" }, status: :not_found
+        end
+
+        def comment_params
+          params.require(:comment).permit(:text)
+        end
+    end
   end
   
 end
