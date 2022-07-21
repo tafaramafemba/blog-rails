d6ede1513d95dc6dae033b99c9ff5e643be02fb5
tafaramafemba <tafara.maf@gmail.com>
2022-07-21 20:52:29 +0200

    create index method for posts controller API

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class PostsController < ApplicationController
       def index
-        
+        @posts = Post.order('created_at DESC')
+        render json: { success: true, message: 'Successfully loaded all posts', data: { posts: @posts } }, status: :ok
       end
     end
   end
