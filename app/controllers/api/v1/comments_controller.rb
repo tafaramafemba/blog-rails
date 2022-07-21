b0b5bd54f8111dab982a29ca758f1317d45a6e81
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-22 01:58:23 +0300

    Fix all linter errors

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,38 +1,36 @@
 module Api
   module V1
     class CommentsController < ApplicationController
-        def index
-          @comments = Comment.where(post_id: params[:post_id]).where(user_id: params[:user_id]).order(:created_at)
-          render json: {
-            success: true, message: "Successfully loaded comments", data: { comments: @comments }
-          }
-        end
+      def index
+        @comments = Comment.where(post_id: params[:post_id]).where(user_id: params[:user_id]).order(:created_at)
+        render json: {
+          success: true, message: "Successfully loaded comments", data: { comments: @comments }
+        }
+      end
 
-        def create
-          @post = Post.find(params[:post_id])
-          @comment = @post.comments.create(comment_params, user_id: params[:user_id])
+      def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params, user_id: params[:user_id])
 
-          if @comment.save
-            render json: { success: true, message: 'Successfully created comment', data: { comment: @comment } }, status: :created
-          else
-            render json: { success: false, errors: @comment.errors }, status: :unprocessable_entity
-          end
+        if @comment.save
+          render json: { success: true, message: 'Successfully created comment', data: { comment: @comment } }, status: :created
+        else
+          render json: { success: false, errors: @comment.errors }, status: :unprocessable_entity
         end
-        rescue ActiveRecord::RecordNotFound
-          no_record(params[:post_id])
-        end
+      end
+      rescue ActiveRecord::RecordNotFound
+        no_record(params[:post_id])
+      end
 
-        private
+      private
 
-        def no_record(post)
-          render json: { success: false, message: "This post: #{post} does not exist" }, status: :not_found
-        end
+      def no_record(post)
+        render json: { success: false, message: "This post: #{post} does not exist" }, status: :not_found
+      end
 
-        def comment_params
-          params.require(:comment).permit(:text)
-        end
+      def comment_params
+        params.require(:comment).permit(:text)
+      end
     end
-  end
-  
-end
+  end 
 
