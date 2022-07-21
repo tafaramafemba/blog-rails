3c46639218dfda63f5ec057ef97978a37bca98cf
tafaramafemba <tafara.maf@gmail.com>
2022-07-21 20:45:10 +0200

    configure api namespace and versions for comments and posts controller

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      def index
+  
+      end
+
+      def create
+        
+      end
+      
+  end
+  end
+  
+end
+
---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+      def index
+        
+      end
+    end
+  end
+end
+
+
